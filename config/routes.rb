ab834cfb664cdb567b929efa47cec09b71bda431
Jon Norton <jmichaeln5@gmail.com>
2018-04-26 17:40:42 -0400

    Added minimal styling + added pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :recipes
-  root 'home#index'
 
+  root 'recipes#index'
+
+  resources :recipes
+  get 'pages/about'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
