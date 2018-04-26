2977306d4f536e826aa9def3708880a26cb47aa6
Jon Norton <jmichaeln5@gmail.com>
2018-04-26 18:23:26 -0400

    Prepping for AWS connection (fog with carrierwave)

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,17 +1,5 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
-  storage :file
-
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-  # version :thumb do
-  #    process :resize_to_fit => [50, 50]
-  #  end
-  #
-  # def extension_white_list
-  #    %w(jpg jpeg gif png)
-  #  end
+  storage :fog
 
 end
