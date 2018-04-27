df9e3a55b64ddae20efa6fb1be04784559399315
Jon Norton <jmichaeln5@gmail.com>
2018-04-27 12:06:56 -0400

    Heroku Deployment

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,7 @@ class RecipesController < ApplicationController
   # GET /recipes
   # GET /recipes.json
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.all.order("created_at DESC")
   end
 
   # GET /recipes/1
