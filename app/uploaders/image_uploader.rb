f201be06d56e29c15ad7b8544a6b0b1234d122c0
Jon Norton <jmichaeln5@gmail.com>
2018-04-27 10:36:19 -0400

    Basic Styling

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,21 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
-  storage :fog
+
+  ##### **** For LocalHost ***** #####
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+  ##### **** For LocalHost ***** #####
+
+
+#########################################################
+
+
+  ##### **** For Heroku ***** #####
+  # storage :fog
+  ##### **** For Heroku ***** #####
+
 
 end
