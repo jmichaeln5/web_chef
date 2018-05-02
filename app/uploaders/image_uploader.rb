70b2c1bdb2cf0ed483d697c1d0fbf8509ffad078
Jon Norton <jmichaeln5@gmail.com>
2018-05-02 15:42:36 -0400

    Heroku prep

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,11 +2,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
 
   ##### **** For LocalHost ***** #####
-  storage :file
-
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # storage :file
+  #
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
   ##### **** For LocalHost ***** #####
 
 
@@ -14,7 +14,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
 
   ##### **** For Heroku ***** #####
-  # storage :fog
+  storage :fog
   ##### **** For Heroku ***** #####
 
 
